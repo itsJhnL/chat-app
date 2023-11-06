c92b7a10a11d60c318178793e699c6ff3af1cfe1
itsLeo <107639845+itsJhnL@users.noreply.github.com>
2023-11-06 23:37:26 +0800

    added mainte

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { Counter } from './features/counter/Counter';
 import './App.css';
-import Chat from './components/Chat';
+// import Chat from './components/Chat';
+import UnderConst from './components/underconstruction/Underconstruction'
 
 function App() {
   return (
     <>
-      <Chat />
+      {/* <Chat /> */}
+      <UnderConst />
     </>
   );
 }
